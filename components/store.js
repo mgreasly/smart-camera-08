9c727a5ffb2b0eb894edac2ce7f92169ec3bccb9
Marcus Greasly <marcus.greasly@avanade.com>
2018-05-02 13:48:53 +1000

    Updated results handling

--- components/store.js
+++ components/store.js
@@ -1,12 +1,13 @@
 import createStore from 'redux-zero';
 import axios from 'axios';
 
-const store = createStore({ results: [] });
+const store = createStore({ results: [], loadingResults: false });
 
-const mapToProps = ({ results }) => ({ results });
+const mapToProps = ({ results, loadingResults }) => ({ results, loadingResults });
 
 const actions = ({ setState }) => ({
     getResults(state, value) {
+        setState({ loadingResults: true });
         return axios.post(
             'http://workshop-ava.azurewebsites.net/api/Camera/RecognizeImage', 
             value
@@ -19,18 +20,12 @@ const actions = ({ setState }) => ({
                 description: product.Products[0].FullDescription,
                 price: product.Products[0].Price
             };
-            var results = state.results.concat([{
-                image: value,
-                product: result
-            }]);
-            return { results: results }
+            var results = [{ image: value, product: result }].concat(state.results);
+            return { results: results, loadingResults: false };
         })
         .catch(error => {
-            var results = state.results.concat([{
-                image: value,
-                product: null
-            }]);
-            return { results: results }
+            var results = [{ image: value, product: null }].concat(state.results);
+            return { results: results, loadingResults: false };
         })
     }
 });
